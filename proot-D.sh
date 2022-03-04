a53e7a42b5d7810fe8513b1d644632be649d0adc
PAYMON7 <93040653+PAYMON7@users.noreply.github.com>
2022-03-04 03:31:54 -0300

    Add files via upload

---
+++ proot-D.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# instalar  proot-distro
+
+echo " _________________________________________"
+echo "|                                         |"
+echo "|          Facebook: metaverso negro      |"
+echo "| Script desenvolvido por meta-verso negro|"
+echo "|_________________________________________|"
+
+echo "instalando proot-distro..."
+apt install proot-distro
+cd
+echo "PROOT-DISTRO INSTALADO"
+
+# perguntar qual sistema quer instalar
+echo""
+echo "_______ Digite o nome como esta escrito abaixo ⸔  _____"
+echo " ________________"
+echo "|               |"
+echo "|      void     |"
+echo "|     ubuntu    |"
+echo "|   archlinux   |"
+echo "|     fedora    |"
+echo "|     alpine    |"
+echo "|    opensuse   |"
+echo "|     debian    |"
+echo "|manjaro-aarch64|"
+echo "|_______________|"
+echo""
+echo -n " Deseja Instalar o qual Distribuição? :"
+read  sistema;
+if  [ "$sistema" == "fedora" ]
+then
+echo " instalando o fedora..."
+proot-distro install fedora
+echo "fedora instalado!"
+echo "executando..."
+proot-distro login fedora
+fi
+if [ "$sistema" == "archlinux" ]
+then
+echo "instalando archlinux..."
+proot-distro install archlinux
+echo "archlinux instalado"
+echo "executando..."
+proot-distro login archlinux
+fi
+if [ "$sistema" == "ubuntu" ]
+then
+echo "instalando ubuntu..."
+proot-distro install ubuntu
+echo "ubuntu instalado"
+echo "executando..."
+proot-distro login ubuntu
+fi
+if [ "$sistema" == "void" ]
+then
+echo "instalando void..."
+proot-distro install void
+echo "void instalado"
+echo "executando..."
+proot-distro login void
+fi
+if [ "$sistema" == "manjaro-aarch64" ]
+then
+echo "instalando manjaro-aarch64..."
+proot-distro install manjaro-aarch64
+echo "manjaro instalado"
+echo "executando..."
+proot-distro login manjaro-aarch64
+fi
+if [ "$sistema" == "alpine" ]
+then
+echo "instalando alpine..."
+proot-distro install alpine
+echo "alpine instalado"
+echo "executando..."
+proot-distro login alpine
+fi
+if [ "$sistema" == "opensuse" ]
+then
+echo "instalando opensuse..."
+proot-distro install opensuse
+echo "opensuse instalado"
+echo "executando..."
+proot-distro login opensuse
+fi
+if [ "$sistema" == "debian" ]
+then
+echo "instalando debian..."
+proot-distro install debian
+echo "debian instalado"
+echo "executando..."
+proot-distro login debian
+fi
+
+echo""
+
+echo "Script finalizado"
+echo "tchau-tchal"
+
+echo""
